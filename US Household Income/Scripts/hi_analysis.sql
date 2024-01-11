cb1fa8893c597f325cb58ee84b249359c64ba6d4
Alan Sampedro <137914886+a-smp@users.noreply.github.com>
2024-01-10 19:27:35 -0500

    Update hi_analysis.sql

--- US Household Income/Scripts/hi_analysis.sql
+++ US Household Income/Scripts/hi_analysis.sql
@@ -9,8 +9,8 @@ DATE 2024-01-08
 
 -- Top 10 largest US States by land area
 SELECT state_name,
-	   SUM(a_land) AS total_a_land,
-	   SUM(a_water) AS total_a_water
+       SUM(a_land) AS total_a_land,
+       SUM(a_water) AS total_a_water
   FROM us_household_income
  GROUP BY state_name
  ORDER BY total_a_land DESC
@@ -20,7 +20,7 @@ SELECT state_name,
 
 -- Top 10 US States by water area
 SELECT state_name,
-	   SUM(a_water) AS total_a_water
+       SUM(a_water) AS total_a_water
   FROM us_household_income
  GROUP BY state_name
  ORDER BY total_a_water DESC
@@ -30,8 +30,8 @@ SELECT state_name,
 
 -- Top 10 lowest median household income states
 SELECT state_name,
-	   ROUND(CAST(AVG(mean) AS NUMERIC), 1) AS avg_household_income,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY median) AS NUMERIC), 1) AS median_household_income
+       ROUND(CAST(AVG(mean) AS NUMERIC), 1) AS avg_household_income,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY median) AS NUMERIC), 1) AS median_household_income
   FROM us_household_income
  WHERE mean != 0
  GROUP BY state_name
@@ -42,8 +42,8 @@ SELECT state_name,
 
 -- Top 10 highest median household income states
 SELECT state_name,
-	   ROUND(CAST(AVG(mean) AS NUMERIC), 1) AS avg_household_income,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY median) AS NUMERIC), 1) AS median_household_income
+       ROUND(CAST(AVG(mean) AS NUMERIC), 1) AS avg_household_income,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY median) AS NUMERIC), 1) AS median_household_income
   FROM us_household_income
  WHERE mean != 0
  GROUP BY state_name
@@ -54,10 +54,10 @@ SELECT state_name,
 
 -- Top 10 highest median cost of living index income states
 SELECT a.state_name,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY b.coli) AS NUMERIC), 1) AS median_coli
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY b.coli) AS NUMERIC), 1) AS median_coli
   FROM us_household a
-  	   JOIN cost_living_index b
-	     ON a.state_ab = b.state
+       JOIN cost_living_index b
+         ON a.state_ab = b.state
  GROUP BY a.state_name
  ORDER BY median_coli DESC
  LIMIT 10
@@ -66,10 +66,10 @@ SELECT a.state_name,
 
 -- Top 10 lowest median cost of living index income states
 SELECT a.state_name,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY b.coli) AS NUMERIC), 1) AS median_coli
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY b.coli) AS NUMERIC), 1) AS median_coli
   FROM us_household a
-  	   JOIN cost_living_index b
-	     ON a.state_ab = b.state
+       JOIN cost_living_index b
+	 ON a.state_ab = b.state
  GROUP BY a.state_name
  ORDER BY median_coli
  LIMIT 10
@@ -78,9 +78,9 @@ SELECT a.state_name,
 
 -- Top 10 highest median employed civilians states in 2022
 SELECT area,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY non_inst) AS NUMERIC), 1) AS median_non_inst,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY employed) AS NUMERIC), 1) AS median_employed,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY employed_ratio) AS NUMERIC), 1) AS median_employed_ratio
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY non_inst) AS NUMERIC), 1) AS median_non_inst,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY employed) AS NUMERIC), 1) AS median_employed,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY employed_ratio) AS NUMERIC), 1) AS median_employed_ratio
   FROM us_unemployment
  WHERE year = 2022
  GROUP BY area
@@ -91,9 +91,9 @@ SELECT area,
 
 -- Top 10 lowest median employed civilians states in 2022
 SELECT area,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY non_inst) AS NUMERIC), 1) AS median_non_inst,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY employed) AS NUMERIC), 1) AS median_employed,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY employed_ratio) AS NUMERIC), 1) AS median_employed_ratio
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY non_inst) AS NUMERIC), 1) AS median_non_inst,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY employed) AS NUMERIC), 1) AS median_employed,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY employed_ratio) AS NUMERIC), 1) AS median_employed_ratio
   FROM us_unemployment
  WHERE year = 2022
  GROUP BY area
@@ -104,9 +104,9 @@ SELECT area,
 
 -- Top 10 highest median unemployed civilians states in 2022
 SELECT area,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY non_inst) AS NUMERIC), 1) AS median_non_inst,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY unemployed) AS NUMERIC), 1) AS median_unemployed,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY unemployed_ratio) AS NUMERIC), 1) AS median_unemployed_ratio
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY non_inst) AS NUMERIC), 1) AS median_non_inst,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY unemployed) AS NUMERIC), 1) AS median_unemployed,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY unemployed_ratio) AS NUMERIC), 1) AS median_unemployed_ratio
   FROM us_unemployment
  WHERE year = 2022
  GROUP BY area
@@ -117,9 +117,9 @@ SELECT area,
 
 -- Top 10 lowest median unemployed civilians states in 2022
 SELECT area,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY non_inst) AS NUMERIC), 1) AS median_non_inst,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY unemployed) AS NUMERIC), 1) AS median_unemployed,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY unemployed_ratio) AS NUMERIC), 1) AS median_unemployed_ratio
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY non_inst) AS NUMERIC), 1) AS median_non_inst,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY unemployed) AS NUMERIC), 1) AS median_unemployed,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY unemployed_ratio) AS NUMERIC), 1) AS median_unemployed_ratio
   FROM us_unemployment
  WHERE year = 2022
  GROUP BY area
@@ -132,42 +132,42 @@ SELECT area,
 WITH
 cost_living_state AS (
 SELECT state,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY coli) AS NUMERIC), 1) AS coli
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY coli) AS NUMERIC), 1) AS coli
   FROM cost_living_index
  GROUP BY state
 ),
 
 household_income_state AS (
 SELECT a.state_ab, a.state_name,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY b.median) AS NUMERIC), 1) AS median_hh_income
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY b.median) AS NUMERIC), 1) AS median_hh_income
   FROM us_household a
-  	   JOIN us_household_income b
-	   	 ON a.id = b.id
+       JOIN us_household_income b
+	 ON a.id = b.id
  WHERE median != 0
  GROUP BY a.state_ab, a.state_name
 ),
 
 unemployment_state AS (
 SELECT area,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY non_inst) AS NUMERIC), 1) AS non_institutional,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY labor_force_ratio) AS NUMERIC), 1) AS labor_force_ratio,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY employed_ratio) AS NUMERIC), 1) AS employed_ratio,
-	   ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY unemployed_ratio) AS NUMERIC), 1) AS unemployed_ratio
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY non_inst) AS NUMERIC), 1) AS non_institutional,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY labor_force_ratio) AS NUMERIC), 1) AS labor_force_ratio,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY employed_ratio) AS NUMERIC), 1) AS employed_ratio,
+       ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY unemployed_ratio) AS NUMERIC), 1) AS unemployed_ratio
   FROM us_unemployment
  WHERE year = 2022
  GROUP BY area
 )
 
 SELECT his.state_name AS state,
-	   CAST(his.median_hh_income AS INTEGER),
-	   cls.coli,
-	   CAST(ues.non_institutional AS INTEGER), 
-	   ues.labor_force_ratio, ues.employed_ratio, ues.unemployed_ratio
+       CAST(his.median_hh_income AS INTEGER),
+       cls.coli,
+       CAST(ues.non_institutional AS INTEGER), 
+       ues.labor_force_ratio, ues.employed_ratio, ues.unemployed_ratio
   FROM household_income_state his
-  	   JOIN cost_living_state cls
-	   	 ON his.state_ab = cls.state
-	   JOIN unemployment_state ues
-	     ON his.state_name = ues.area
+       JOIN cost_living_state cls
+	 ON his.state_ab = cls.state
+       JOIN unemployment_state ues
+	 ON his.state_name = ues.area
 ;
 
 
@@ -175,10 +175,10 @@ SELECT his.state_name AS state,
 WITH
 household_income_ny AS (
 SELECT a.state_ab, a.state_name, a.city,
-	   ROUND(CAST(AVG(b.median) AS NUMERIC), 1 ) AS median_household_income
+       ROUND(CAST(AVG(b.median) AS NUMERIC), 1 ) AS median_household_income
   FROM us_household a
-  	   JOIN us_household_income b
-	   	 ON a.id = b.id
+       JOIN us_household_income b
+	 ON a.id = b.id
  WHERE median != 0
    AND state_ab = 'NY'
  GROUP BY a.state_ab, a.state_name, a.city
@@ -190,8 +190,8 @@ SELECT a.state_name, a.city, a.median_household_income, b.coli
   			   FROM cost_living_index
  			  WHERE state = 'NY'
 			 ) b
-		 ON a.state_ab = b.state
-		AND a.city = b.city
+	 ON a.state_ab = b.state
+	AND a.city = b.city
  WHERE median_household_income != 300000.0 --> Filler values
  ORDER BY median_household_income DESC
 ;
